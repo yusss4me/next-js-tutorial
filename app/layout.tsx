5038f39c34f691b3ccf5f839822c769bc421d7b6
ArdiYtrr <ardiyustiar212@gmail.com>
2026-04-03 21:05:56 +0700

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { intern } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://nextjs-tutorial-nu-neon-57.vercel.app/'),
+}
 
 export default function RootLayout({
   children,
